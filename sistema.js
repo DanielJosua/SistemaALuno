a29cc9430de2af48c1c7eeed9ff4ed643f9eb2a9
daniel <daniel.jch1896@gmail.com>
2021-12-09 19:32:29 -0300

    subindo codigo da aula presencial

---
+++ sistema.js
@@ -0,0 +1,55 @@
+//for in e of
+let nomeDeAluno = ["Marcel", "Franklin", "Rita", "Afranio"]
+
+for (let nome of nomeDeAluno) {
+    console.log(`Esta pessoa se chama ${nome}`)
+}
+
+//while ao contrario
+// let numeroDeAlunos = 10;
+
+// let contador = 0
+
+// while(contador <= numeroDeAlunos){
+//     //console.log(contador)
+
+//     if(contador == 0){
+//         console.log("O numero atual é zero")
+//     }else if(contador %2 != 0){
+//         console.log("O numero " + contador + " é impar")
+//     }else{
+//         console.log(`O numero ${contador} é par`)
+//     }
+//     contador++;
+// }   
+
+//while para repetir
+// let numeroDeAlunos = 10;
+
+// let contador = 0
+
+// while(contador <= numeroDeAlunos){
+//     //console.log(contador)
+
+//     if(contador == 0){
+//         console.log("O numero atual é zero")
+//     }else if(contador %2 == 0){
+//         console.log("O numero " + contador + " é par")
+//     }else{
+//         console.log(`O numero ${contador} é Impar`)
+//     }
+//     contador++;
+// }
+
+//for para repetir
+// for (let contador = 0; contador <= numeroDeAlunos; contador++) {
+//     //console.log(contador)
+
+//     if(contador == 0){
+//         console.log("O numero atual é zero")
+//     }else if(contador %2 == 0){
+//         console.log("O numero " + contador + " é par")
+//     }else{
+//         console.log(`O numero ${contador} é Impar`)
+//     }
+// }
